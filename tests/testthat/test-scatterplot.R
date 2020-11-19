5a0e0ce827c1b52d7f6c4b0afcbc50d410206ad9
Tian-Liu-Alice <71460026+Tian-Liu-Alice@users.noreply.github.com>
2020-11-19 15:07:48 +0800

    Update test-scatterplot.R

--- tests/testthat/test-scatterplot.R
+++ tests/testthat/test-scatterplot.R
@@ -7,3 +7,6 @@ test_that("Dataset with no NA", {
   expect_error(scatterplot(test, x, y, 2), "I am sorry, argument \"colour\" should be the name of a colour, which is a character with quotes")
   expect_error(scatterplot(test, x, y, na.rm = 1), "I am sorry, argument \"na.rm\" should be \"TRUE\" or \"FALSE\"")
 })
+# The output should be a ggplot
+# The argument “colour” should be a character object with quotes and argument “na.rm” should be a bool object. 
+# If users input an object with wrong type, the error message will jump out and help them change their input to correct type.
